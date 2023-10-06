8bef138c0d3671fe0c1611b82508e846b59ff6c3
Kevin <kleinkevin6@web.de>
2023-10-06 04:19:07 +0200

    all

---
+++ src/main/java/com/example/integration/IntegrationApplication.java
@@ -0,0 +1,20 @@
+package com.example.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationApplication {
+
+	public static void main(String[] args) {
+
+			SpringApplication.run(IntegrationApplication.class, args);
+			System.out.println("hi");
+
+	}
+	public int calc(int a, int b){
+
+			return a*b;
+	}
+
+}
---
+++ src/test/java/com/example/integration/IntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+			IntegrationApplication a =new IntegrationApplication();
+			assert a.calc(1,2) == 2;
+	}
+
+
+}
